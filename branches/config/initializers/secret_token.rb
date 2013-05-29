05b4bf897f2961ff511fde20c289858a7e56826c
Chen.ruijie@gmail.com <Chen.ruijie@gmail.com@5487d960-7cbe-4de4-d32b-2f06e709aaf1>
2013-05-29 03:28:14 +0000

    初始化数据库迁移项目

---
+++ branches/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Migration::Application.config.secret_token = '51e039af796e38a59e7b1cb8d3bf08eb6513da6c11ab47f21334d88b022a052161be3a41d7db8cf8a2787df77fcd3aa05f48dbb76ae864cccf041383d03891ac'
